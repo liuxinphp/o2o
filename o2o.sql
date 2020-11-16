42f110898b1bc4db3e5d03f67582735e42c63b9d
liuxin <jh3513lx@163.com>
2020-11-16 17:34:42 +0800

    master

--- o2o.sql
+++ o2o.sql
@@ -58,14 +58,125 @@ create table if not exists `o2o_bis`(
     `status` tinyint(1) not null default 0,
     `create_time` int(11) unsigned not null default 0,
     `update_time` int(11) unsigned not null default 0,
-    key city_id('city_id'),
-    key name('name')
-)ENGINE=INNODB auto_increment DEFAULT CHARSET=utf8;
+    key city_id(`city_id`),
+    key name(`name`)
+)ENGINE=INNODB auto_increment=1 DEFAULT CHARSET=utf8;
 
 #商户账户表
 create table if not exists `o2o_bis_account`(
-    
-);
+    `id` int(11) not null primary key auto_increment,
+    `username` varchar(50) not null default '',
+    `password` char(32) not null default '',
+    `code` varchar(10) not null default '',
+    `bis_id` int(11) unsigned not null default 0,
+    `last_login_ip` varchar(20) not null default '',
+    `last_login_time` int(11) unsigned not null default 0,
+    `is_main` tinyint(1) unsigned not null default 0,
+    `listOrder` int(8) unsigned not null default 0,
+    `status` tinyint(1) not null default 0,
+    `create_time` int(11) unsigned not null default 0,
+    `update_time` int(11) unsigned not null default 0,
+    key bis_id(`bis_id`),
+    key username(`username`)
+)ENGINE=INNODB auto_increment=1 DEFAULT CHARSET=utf8;
+
+#商户门店表
+create table if not exists `o2o_bis_location`(
+    `id` int(11) not null primary key auto_increment,
+    `name` varchar(50) not null default '',
+    `logo` varchar(255) not null default '',
+    `address` varchar(255) not null default'',
+    `tel` varchar(20) not null default '',
+    `contact` varchar(20) not null default '',
+    `xpoint` varchar(20) not null default '',
+    `ypoint` varchar(20) not null default '',
+    `bis_id` int(11) unsigned not null default 0,
+    `open_time` int(11) unsigned not null default 0,
+    `content` text not null,
+    `is_main` tinyint(1) unsigned not null default 0,
+    `api_address` varchar(255) not null default '',
+    `city_id` int(11) unsigned not null default 0,
+    `city_path` varchar(50) not null default '',
+    `category_id` int(11) unsigned not null default 0,
+    `category_path` varchar(50) not null default '',
+    `bank_info` varchar(50) not null default '',
+    `listOrder` int(8) unsigned not null default 0,
+    `status` tinyint(1) not null default 0,
+    `create_time` int(11) unsigned not null default 0,
+    `update_time` int(11) unsigned not null default 0,
+    key city_id(`city_id`),
+    key bis_id(`bis_id`),
+    key category_id(`category_id`),
+    key name(`name`)
+)ENGINE=INNODB auto_increment=1 CHARSET=utf8;
+
+#商品表
+create table `o2o_deal`(
+    `id` int(11) not null primary key auto_increment,
+    `name` varchar(100) not null default '',
+    `category_id` int(10) not null default 0,
+    `se_category_id` int(11) not null default 0,
+    `bis_id` int(11) not null default 0,
+    `location_ids` int(11) not null default 0,
+    `image` varchar(200) not null default 0,
+    `description` text not null,
+    `start_time` int(10) not null default 0,
+    `end_time` int(10) not null default 0,
+    `origin_price` decimal(20,2) not null default '0.00',
+    `current_price` decimal(20,2) not null default '0.00',
+    `city_id` int(11) not null default 0,
+    `buy_count` int(11) not null default 0,
+    `total_count` int(11) not null default 0,
+    `coupons_begin_time` int(10) not null default 0,
+    `coupons_end_time` int(10) not null default 0,
+    `xpoint` varchar(20) not null default 0,
+    `ypoint` varchar(20) not null default 0,
+    `bis_account_id` int(10) not null default 0,
+    `balance_price` decimal(20,2) not null default '0.00',
+    `notes` text not null,
+    `listOrder` int(8) unsigned not null default 0,
+    `status` tinyint(1) not null default 0,
+    `create_time` int(11) unsigned not null default 0,
+    `update_time` int(11) unsigned not null default 0,
+    key category_id(`category_id`),
+    key se_category_id(`se_category_id`),
+    key city_id(`city_id`),
+    key start_time(`start_time`),
+    key end_time(`end_time`)
+)ENGINE=INNODB auto_increment=1 CHARSET=utf8;
+
+#用户表
+create table `o2o_user`(
+    `id` int(11) not null primary key auto_increment,
+    `username` varchar(30) not null default '',
+    `password` char(32) not null default '',
+    `code` varchar(10) not null default '',
+    `last_login_ip` varchar(20) not null default '',
+    `last_login_time` int(11) unsigned not null default 0,
+    `email` varchar(30) not null default '',
+    `mobile` varchar(30) not null default '',
+    `listOrder` int(8) unsigned not null default 0,
+    `status` tinyint(1) not null default 0,
+    `create_time` int(10) unsigned not null default 0,
+    `update_time` int(10) unsigned not null default 0,
+    unique key username(`username`),
+    unique key email(`email`)
+)ENGINE=INNODB auto_increment=1 CHARSET=utf8;
+
+#推荐表
+create table if not exists `o2o_featured`(
+    `id` int(11) unsigned primary key auto_increment,
+    `type` tinyint(1) not null default 0,
+    `title` varchar(30) not null default '',
+    `image` varchar(255) not null default '',
+    `url` varchar(255) not null default '',
+    `description` varchar(255) not null default '',
+    `listOrder` int(8) unsigned not null default 0,
+    `status` tinyint(1) not null default 0,
+    `create_time` int(10) unsigned not null default 0,
+    `update_time` int(10) unsigned not null default 0
+)ENGINE=INNODB auto_increment=1 CHARSET=utf8;
+
 
 
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,76 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '30a3c39ed66a10229052c57055a9f392ac421eab',
+    'name' => 'topthink/think',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f9eee5cec93dfb313a38b6b288741e84e53f02d5',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+    'topthink/framework' => 
+    array (
+      'pretty_version' => 'v5.1.40',
+      'version' => '5.1.40.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df0f92d58c70207c9ce9526fa7743eeff0831328',
+    ),
+    'topthink/think' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30a3c39ed66a10229052c57055a9f392ac421eab',
+    ),
+    'topthink/think-captcha' => 
+    array (
+      'pretty_version' => 'v1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c55455df26a1626a60d0dc35d2d89002b741d44',
+    ),
+    'topthink/think-installer' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52f4b717a46fd00bccd47c5a9f10a5c56d29fcf9',
+    ),
+  ),
+);
