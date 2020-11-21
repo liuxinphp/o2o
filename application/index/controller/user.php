ca6ae4995af5d2915b3558f187df7c964eb4d20f
liuxin <jh3513lx@163.com>
2020-11-21 10:54:00 +0800

    master

---
+++ application/index/controller/user.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class User extends Controller
+{
+    //登录
+    public function login()
+    {
+        return $this->fetch();
+    }
+    //注册
+    public function register()
+    {
+        return $this->fetch();
+    }
+}
