fd7049c11c0bd6706ad5da13582ca060cbdd5dc4
liuxin <jh3513lx@163.com>
2021-01-19 17:35:28 +0800

    master

--- application/bis/controller/Login.php
+++ application/bis/controller/Login.php
@@ -3,11 +3,28 @@ namespace app\bis\controller;
 use think\Controller;
 class Login extends Controller{
     public function index(){
-        if(!request()->isPost()){
-            $this->error('请求错误');
-        }
+        if(request()->isPost()){
         $data=input('post.');
+        //查询用户信息
+        $bis = model('BisAccount')->get(['username'=>$data['username']]);
 
+        if(!$bis || $bis->status!=1){
+            $this->error('用户不存在或未审核通过');
+        }
+        if($bis->password != md5($data['password'].$bis->code)){
+            $this->error('密码错误');
+        }
+        model('BisAccount')->updateById(['last_login_time'=>time()], $bis->id);
+        session('BisAccount',$bis,'bis');
+        return $this->success('登录成功');
+    }else{
+        //获取session
+        $account = session('BisAccount','','bis');
+        if($account && $account->id){
+            return $this->redirect(url('index/index'));
+        }
+        return $this->fetch();
+    }
         
     }
 }
